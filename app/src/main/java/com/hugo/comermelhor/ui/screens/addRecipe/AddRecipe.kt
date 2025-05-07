d9d83152345f5d6b9c745c0e4f5f1080e4c8061e
Hugo Diniz <hhldiniz@gmail.com>
2025-05-07 10:34:32 +0200

    Adição de AppBar na tela de adição de receita

--- app/src/main/java/com/hugo/comermelhor/ui/screens/addRecipe/AddRecipe.kt
+++ app/src/main/java/com/hugo/comermelhor/ui/screens/addRecipe/AddRecipe.kt
@@ -10,13 +10,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -33,6 +37,7 @@ import com.hugo.comermelhor.ui.widgets.ErrorViewType
 import com.hugo.comermelhor.ui.widgets.Loading
 import com.hugo.comermelhor.ui.widgets.PrimaryButton
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddRecipeScreen(
     navController: NavController,
@@ -40,68 +45,81 @@ fun AddRecipeScreen(
     addRecipeViewModel: AddRecipeViewModel = viewModel()
 ) {
     val state by addRecipeViewModel.uiState.collectAsState()
-    Column(
-        modifier = modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceBetween
-    ) {
-        if (state.isLoading) {
-            Loading()
-        } else {
-            if (state.error != null) {
-                Error(
-                    errorViewType = ErrorViewType.Retry,
-                    message = stringResource(R.string.add_recipe_error_msg)
-                )
+    Scaffold(topBar = {
+        TopAppBar(
+            title = { Text(stringResource(R.string.add_recipe_screen_title)) },
+            navigationIcon = { IconButton(onClick = {navController.navigateUp()}) {
+                Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "")
+            } })
+    }) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(it),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            if (state.isLoading) {
+                Loading()
             } else {
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp)
-                ) {
-                    Card(modifier = Modifier.fillMaxWidth()) {
-                        Column(
-                            Modifier
-                                .padding(8.dp)
+                if (state.error != null) {
+                    Error(
+                        errorViewType = ErrorViewType.Retry,
+                        message = stringResource(R.string.add_recipe_error_msg)
+                    )
+                } else {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    ) {
+                        Card(modifier = Modifier.fillMaxWidth()) {
+                            Column(
+                                Modifier
+                                    .padding(8.dp)
+                            ) {
+                                OutlinedTextField(
+                                    state.description,
+                                    label = { Text(stringResource(R.string.description_field_label)) },
+                                    modifier = Modifier.fillMaxWidth(),
+                                    onValueChange = addRecipeViewModel::updateDescription
+                                )
+                                OutlinedTextField(
+                                    state.preparation,
+                                    singleLine = false,
+                                    label = { Text(stringResource(R.string.preparation_field_label)) },
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .height(200.dp),
+                                    onValueChange = addRecipeViewModel::updatePreparation
+                                )
+                            }
+                        }
+
+                        IngredientsSection(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(400.dp)
+                                .padding(top = 16.dp),
+                            state = state,
+                            addRecipeViewModel = addRecipeViewModel
+                        )
+                    }
+
+                    Column {
+                        Text(stringResource(R.string.calories, state.calories))
+
+                        PrimaryButton(
+                            text = stringResource(R.string.add_recipe_btn),
+                            modifier = Modifier.padding(bottom = 16.dp)
                         ) {
-                            OutlinedTextField(
-                                state.description,
-                                label = { Text(stringResource(R.string.description_field_label)) },
-                                modifier = Modifier.fillMaxWidth(),
-                                onValueChange = addRecipeViewModel::updateDescription
-                            )
-                            OutlinedTextField(
-                                state.preparation,
-                                singleLine = false,
-                                label = { Text(stringResource(R.string.preparation_field_label)) },
-                                modifier = Modifier.fillMaxWidth().height(200.dp),
-                                onValueChange = addRecipeViewModel::updatePreparation
-                            )
+                            addRecipeViewModel.addRecipe()
                         }
                     }
 
-                    IngredientsSection(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(400.dp)
-                            .padding(top = 16.dp),
-                        state = state,
-                        addRecipeViewModel = addRecipeViewModel
-                    )
                 }
-
-                Column {
-                    Text(stringResource(R.string.calories, state.calories))
-
-                    PrimaryButton(
-                        text = stringResource(R.string.add_recipe_btn),
-                        modifier = Modifier.padding(bottom = 16.dp)
-                    ) {
-                        addRecipeViewModel.addRecipe()
-                    }
-                }
-
             }
+
         }
 
     }
@@ -142,19 +160,25 @@ private fun IngredientsSection(
             items(state.ingredients) { ingredient ->
                 Row {
                     OutlinedTextField(
-                        modifier = Modifier.fillMaxWidth(0.6f).padding(horizontal = 4.dp),
+                        modifier = Modifier
+                            .fillMaxWidth(0.6f)
+                            .padding(horizontal = 4.dp),
                         value = ingredient.name,
                         label = { Text(stringResource(R.string.ingredient_description_label)) },
                         onValueChange = { addRecipeViewModel.updateIngredient(ingredient.copy(name = it)) }
                     )
                     OutlinedTextField(
-                        modifier = Modifier.fillMaxWidth(0.45f).padding(horizontal = 4.dp),
+                        modifier = Modifier
+                            .fillMaxWidth(0.45f)
+                            .padding(horizontal = 4.dp),
                         value = ingredient.amount.toString(),
                         label = { Text(stringResource(R.string.ingredient_amount_description_label)) },
                         onValueChange = { addRecipeViewModel.updateIngredient(ingredient.copy(amount = it.toFloat())) }
                     )
                     OutlinedTextField(
-                        modifier = Modifier.fillMaxWidth(1f).padding(horizontal = 4.dp),
+                        modifier = Modifier
+                            .fillMaxWidth(1f)
+                            .padding(horizontal = 4.dp),
                         value = ingredient.unit,
                         label = { Text(stringResource(R.string.ingredient_unit_description_label)) },
                         onValueChange = { addRecipeViewModel.updateIngredient(ingredient.copy(unit = it)) }
