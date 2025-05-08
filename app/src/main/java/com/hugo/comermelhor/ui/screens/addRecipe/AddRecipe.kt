bbc08e1495413dfd7f428e796a648be1a44f2728
Hugo Diniz <hhldiniz@gmail.com>
2025-05-08 10:54:30 +0200

    Refatoração da view dos ingredientes

--- app/src/main/java/com/hugo/comermelhor/ui/screens/addRecipe/AddRecipe.kt
+++ app/src/main/java/com/hugo/comermelhor/ui/screens/addRecipe/AddRecipe.kt
@@ -16,7 +16,6 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
@@ -233,7 +232,6 @@ private fun IngredientsSection(
                                 }
                             )
                         }
-                        HorizontalDivider(modifier = Modifier.padding(8.dp))
                     }
                 }
             }
